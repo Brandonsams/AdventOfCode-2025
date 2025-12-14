82d52fc1c82b71b9dd52d8dc36f1a4a6a7d3fe75
brandonsams <brandonkeithsams@gmail.com>
2025-12-14 13:35:19 -0700

    Completed Day01 Part 1

---
+++ Day01/part1.py
@@ -0,0 +1,24 @@
+answer = 0 
+day = 1
+fname = "example.txt"
+fname = "input.txt"
+fpath = f"./Day{day:02d}/{fname}"
+
+with open(fpath) as f:
+    lines = [line.rstrip("\n") for line in f]
+
+dial_size = 100
+position = 50
+
+for line in lines:
+    direction = -1 if line[0] == "L" else 1
+    amount = int(line[1:])
+    
+    for i in range(amount):
+        position = (position + direction) % dial_size
+    
+    if position == 0:
+        answer += 1
+
+print(answer)
+    
